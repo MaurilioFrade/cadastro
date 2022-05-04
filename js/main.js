7d80f563de7e27545ba50675605bb00e7ef3a4fd
Maurílio Frade <mauriliofelipeee@gmail.com>
2022-05-03 21:49:41 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-function cadastrar(){
-    let ListaUser = JSON.parse(localStorage.getItem('ListaUser')|| '[]')
+function cadastrar() {
+    let ListaUser = JSON.parse(localStorage.getItem('ListaUser') || '[]')
 
     ListaUser.push(
         {
-    nome : document.getElementById("firshname").value,
-    sobrenome : document.getElementById("lastname").value,
-    vemail : document.getElementById("email").value,
-    senha : document.getElementById("number").value,
-    confSenha : document.getElementById("password").value,
-    celular : document.getElementById("confirmpassword").value,
-    genero : document.getElementById("genero").value
+            nome: document.getElementById("firshname").value,
+            sobrenome: document.getElementById("lastname").value,
+            vemail: document.getElementById("email").value,
+            senha: document.getElementById("number").value,
+            confSenha: document.getElementById("password").value,
+            celular: document.getElementById("confirmpassword").value,
+            genero: document.getElementById("genero").value
         }
     )
 
     localStorage.setItem('ListaUser', JSON.stringify(ListaUser))
+
+    //limpar campos
+    document.getElementById("firshname").value = ""
+    document.getElementById("lastname").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("number").value = ""
+    document.getElementById("password").value = ""
+    document.getElementById("confirmpassword").value = ""
 }

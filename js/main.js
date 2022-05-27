1e6df8f016d1a7b160a9f4e219b0b7c755963c42
Maurílio Frade <mauriliofelipeee@gmail.com>
2022-05-27 19:24:54 -0300

    mudança final

--- js/main.js
+++ js/main.js
@@ -1,25 +1,117 @@
-function cadastrar() {
-    let ListaUser = JSON.parse(localStorage.getItem('ListaUser') || '[]')
+const LOGIN_URL = "login.html";
 
-    ListaUser.push(
-        {
-            nome: document.getElementById("firshname").value,
-            sobrenome: document.getElementById("lastname").value,
-            email: document.getElementById("email").value,
-            senha: document.getElementById("number").value,
-            confSenha: document.getElementById("password").value,
-            celular: document.getElementById("confirmpassword").value,
-            genero: document.getElementById("genero").value
+
+var db_usuarios = {};
+
+
+var usuarioCorrente = {};
+
+
+function generateUUID() { 
+    var d = new Date().getTime();
+    var d2 = (performance && performance.now && (performance.now()*1000)) || 0;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;
+        if(d > 0){
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
         }
-    )
-
-    localStorage.setItem('ListaUser', JSON.stringify(ListaUser))
-
-    //limpar campos
-    document.getElementById("firshname").value = "",
-        document.getElementById("lastname").value = "",
-        document.getElementById("email").value = "",
-        document.getElementById("number").value = "",
-        document.getElementById("password").value = "",
-        document.getElementById("confirmpassword").value = ""
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
 }
+
+
+
+const dadosIniciais = {
+    usuarios: [
+        { "id": generateUUID (), "login": "admin", "senha": "123", "nome": "Administrador do Sistema", "email": "admin@abc.com"},
+        { "id": generateUUID (), "login": "user", "senha": "123", "nome": "Usuario Comum", "email": "user@abc.com"},
+       
+    ]
+};
+
+
+function initLoginApp () {
+    
+    usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
+    if (usuarioCorrenteJSON) {
+        usuarioCorrente = JSON.parse (usuarioCorrenteJSON);
+    }
+    
+   
+    var usuariosJSON = localStorage.getItem('db_usuarios');
+
+    
+    if (!usuariosJSON) {  //se não tem dados
+        
+       
+        alert('Dados de usuários não encontrados no localStorage. \n -----> Fazendo carga inicial.');
+
+        
+        db_usuarios = dadosIniciais;
+
+        
+        localStorage.setItem('db_usuarios', JSON.stringify (dadosIniciais));
+    }
+    else  {  // Se tem dados 
+        
+        
+        db_usuarios = JSON.parse(usuariosJSON);    
+    }
+};
+
+
+// Verifica se o login do usuário está positivo
+function loginUser (login, senha) {
+    
+    
+    for (var i = 0; i < db_usuarios.usuarios.length; i++) {
+        var usuario = db_usuarios.usuarios[i];
+        
+       
+        if (login == usuario.login && senha == usuario.senha) {
+            usuarioCorrente.id = usuario.id;
+            usuarioCorrente.login = usuario.login;
+            usuarioCorrente.email = usuario.email;
+            usuarioCorrente.nome = usuario.nome;
+            
+            
+            sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+
+           
+            return true;
+        }
+    }
+
+    //falso
+    return false;
+}
+
+
+function logoutUser () {
+    usuarioCorrente = {};
+    sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+    window.location = LOGIN_URL;
+}
+
+function addUser (nome, login, senha, email) {
+    
+   
+    let newId = generateUUID ();
+    let usuario = { "id": newId, "login": login, "senha" : senha, "nome": nome, "email": email };
+    
+   
+    db_usuarios.usuarios.push (usuario);
+
+    localStorage.setItem('db_usuarios', JSON.stringify (db_usuarios));
+}
+
+function setUserPass () {
+
+}
+
+
+initLoginApp ();
